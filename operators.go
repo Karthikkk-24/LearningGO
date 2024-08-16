d2a07a41a6380812f88d40a56f3240fc6be18968
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-16 15:36:59 +0530

    Added all operators

--- operators.go
+++ operators.go
@@ -23,4 +23,47 @@ func operators() {
 	f := b / a
 	fmt.Println(f)
 
+	// modulus
+	g := a % b
+	fmt.Println(g)
+
+	// increment
+	h := a
+	h++
+	fmt.Println(h)
+
+	// decrement
+	i := b
+	i--
+	fmt.Println(i)
+
+	// assignment
+	j := 10
+	j += 5
+	fmt.Println(j)
+
+	// comparison
+	k := 10
+	l := 5
+	m := k > l
+	fmt.Println(m)
+
+	// logical
+	n := true
+	o := false
+	p := n && o
+	fmt.Println(p)
+
+	// bitwise
+	q := 10
+	r := 5
+	s := q & r
+	fmt.Println(s)
+
+	// string concatenation
+	t := "Hello"
+	u := "World"
+	v := t + " " + u
+	fmt.Println(v)
+
 }
