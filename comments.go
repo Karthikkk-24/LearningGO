05d3ead8600b93ae9b2ae803de515c9372edb887
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-16 10:58:52 +0530

    Variable Declaration adn comments in GO lang

---
+++ comments.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func comments() {
+	fmt.Println("Comments in GO!")
+
+	fmt.Println("Single Line Comment")
+	// This is a comment
+
+	fmt.Println("Multi Line Comment")
+	/*
+		Multi
+		Line
+		Comment
+	*/
+
+}
